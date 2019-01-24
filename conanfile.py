d77f1a2b37214b8c38194f5d7bd5eaca11b39036
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-01-23 19:11:24 -0600

    Setup conan scripts

---
+++ conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+
+
+class SquashfuseConan(ConanFile):
+    name = "fuse"
+    version = "2.9.9"
+    license = "https://github.com/libfuse/libfuse/blob/master/LICENSE"
+    author = "Alexis Lopez Zubieta <contact@azubieta.net>"
+    url = "https://github.com/azubieta/conan-libfuse"
+    description = "libfuse conan package for AppImage "
+    topics = ("fuse")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        tools.download("https://github.com/libfuse/libfuse/releases/download/fuse-2.9.9/fuse-2.9.9.tar.gz",
+                       "fuse-2.9.9.tar.gz")
+        tools.untargz("fuse-2.9.9.tar.gz")
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.fpic = True
+        env_build_vars = autotools.vars
+        autotools.configure(configure_dir="fuse-2.9.9", vars=env_build_vars,
+                            args=["--disable-util", "--disable-example", "--verbose",])
+        autotools.make(vars=env_build_vars)
+        autotools.install(vars=env_build_vars)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="fuse-2.9.9/include/")
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["fuse"]
